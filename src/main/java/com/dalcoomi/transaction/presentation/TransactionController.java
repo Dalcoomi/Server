83e5baa651b2549497da3942fb047015018f5b32
fakerdeft <whakswp123@naver.com>
2025-05-12 20:59:48 +0900

    (#8)feat: 내 거래 조회 시 수입/지출/총합 계산 기능 추가

--- src/main/java/com/dalcoomi/transaction/presentation/TransactionController.java
+++ src/main/java/com/dalcoomi/transaction/presentation/TransactionController.java
@@ -3,8 +3,6 @@ package com.dalcoomi.transaction.presentation;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
-import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,8 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.dalcoomi.auth.config.AuthMember;
 import com.dalcoomi.transaction.application.TransactionService;
-import com.dalcoomi.transaction.dto.TransactionInfo;
-import com.dalcoomi.transaction.dto.request.CreateMyTransactionRequest;
+import com.dalcoomi.transaction.domain.Transaction;
+import com.dalcoomi.transaction.dto.TransactionsInfo;
+import com.dalcoomi.transaction.dto.request.TransactionRequest;
 import com.dalcoomi.transaction.dto.response.GetMyTransactionResponse;
 
 import lombok.RequiredArgsConstructor;
@@ -30,20 +29,19 @@ public class TransactionController {
 
 	@PostMapping("/my")
 	@ResponseStatus(CREATED)
-	public void createMyTransaction(@AuthMember Long memberId, @RequestBody CreateMyTransactionRequest request) {
-		TransactionInfo transactionInfo = TransactionInfo.from(request);
+	public void createMyTransaction(@AuthMember Long memberId, @RequestBody TransactionRequest request) {
+		Transaction transaction = Transaction.from(request);
 
-		transactionService.createTransaction(memberId, transactionInfo);
+		transactionService.createTransaction(memberId, request.categoryId(), transaction);
 	}
 
 	@GetMapping("/my")
 	@ResponseStatus(OK)
 	public GetMyTransactionResponse getMyTransactionsWithYearAndMonth(@AuthMember Long memberId,
 		@RequestParam("year") Integer year, @RequestParam("month") Integer month) {
-		List<TransactionInfo> transactionInfos = transactionService.getTransactionsByMemberIdAndYearAndMonth(memberId,
-			year,
+		TransactionsInfo transactionsInfo = transactionService.getTransactionsByMemberIdAndYearAndMonth(memberId, year,
 			month);
 
-		return GetMyTransactionResponse.from(transactionInfos);
+		return GetMyTransactionResponse.from(transactionsInfo);
 	}
 }
