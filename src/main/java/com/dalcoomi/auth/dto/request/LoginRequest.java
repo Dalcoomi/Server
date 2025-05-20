8c208fa4655091398662f3dca2b494a64d117a01
fakerdeft <whakswp123@naver.com>
2025-05-20 18:21:38 +0900

    (#7)feat: not null 검증 추가

--- src/main/java/com/dalcoomi/auth/dto/request/LoginRequest.java
+++ src/main/java/com/dalcoomi/auth/dto/request/LoginRequest.java
@@ -2,8 +2,13 @@ package com.dalcoomi.auth.dto.request;
 
 import com.dalcoomi.member.domain.SocialType;
 
+import jakarta.validation.constraints.NotNull;
+
 public record LoginRequest(
+	@NotNull(message = "소셜 ID는 필수입니다.")
 	String socialId,
+
+	@NotNull(message = "소셜 타입은 필수입니다.")
 	SocialType socialType
 ) {
 
