a35703da901061c1c5973f875e888ff3e144d6db
fakerdeft <whakswp123@naver.com>
2025-08-04 16:22:04 +0900

    (#69)refactor: 유효값 공용 상수로 수정

---
+++ src/main/java/com/dalcoomi/common/constant/MemberConstants.java
@@ -0,0 +1,16 @@
+package com.dalcoomi.common.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class MemberConstants {
+
+	public static final int EMAIL_LENGTH = 100;
+	public static final int NAME_MIN_LENGTH = 2;
+	public static final int NAME_MAX_LENGTH = 30;
+	public static final int NICKNAME_MIN_LENGTH = 2;
+	public static final int NICKNAME_MAX_LENGTH = 35;
+	public static final int GENDER_LENGTH = 2;
+	public static final int PROFILE_IMAGE_URL_LENGTH = 255;
+}
