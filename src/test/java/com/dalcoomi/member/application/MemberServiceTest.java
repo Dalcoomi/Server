577b9046c10515f5cc6ea1679b64ff529afb9998
fakerdeft <whakswp123@naver.com>
2025-08-05 14:48:47 +0900

    (#69)feat: 이름, 이메일, 성별 검증 실패 케이스 추가

--- src/test/java/com/dalcoomi/member/application/MemberServiceTest.java
+++ src/test/java/com/dalcoomi/member/application/MemberServiceTest.java
@@ -1,5 +1,7 @@
 package com.dalcoomi.member.application;
 
+import static com.dalcoomi.common.error.model.ErrorMessage.MEMBER_INVALID_EMAIL;
+import static com.dalcoomi.common.error.model.ErrorMessage.MEMBER_INVALID_GENDER;
 import static com.dalcoomi.common.error.model.ErrorMessage.MEMBER_INVALID_NAME;
 import static com.dalcoomi.member.domain.SocialType.KAKAO;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -38,6 +40,130 @@ class MemberServiceTest {
 	private MemberRepository memberRepository;
 
 	@Test
+	@DisplayName("이메일 공백으로 인해 회원가입 실패")
+	void email_null_sign_up_over_fail() {
+		// given
+		String socialId = "12345";
+		SocialType socialType = KAKAO;
+		String email = "";
+		String name = "프라이인드로스테쭈젠댄마리소피아수인레나테엘리자벳피아루이제";
+		LocalDate birthday = LocalDate.of(1990, 1, 1);
+		String gender = "남성";
+		boolean serviceAgreement = true;
+		boolean collectionAgreement = true;
+
+		MemberInfo memberInfo = MemberInfo.builder()
+			.email(email)
+			.name(name)
+			.birthday(birthday)
+			.gender(gender)
+			.serviceAgreement(serviceAgreement)
+			.collectionAgreement(collectionAgreement)
+			.build();
+
+		SocialInfo socialInfo = SocialInfo.builder()
+			.socialId(socialId)
+			.socialType(socialType)
+			.memberInfo(memberInfo)
+			.build();
+
+		given(socialConnectionRepository.existsMemberBySocialIdAndSocialType(socialId, socialType)).willReturn(false);
+
+		// when & then
+		assertThatThrownBy(() -> memberService.signUp(socialInfo))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(MEMBER_INVALID_EMAIL.getMessage());
+
+		// verify
+		then(socialConnectionRepository).should().existsMemberBySocialIdAndSocialType(socialId, socialType);
+		then(memberRepository).should(never()).save(any(Member.class));
+		then(socialConnectionRepository).should(never()).save(any(SocialConnection.class));
+	}
+
+	@Test
+	@DisplayName("이메일 길이 초과로 인해 회원가입 실패")
+	void email_length_over_sign_up_over_fail() {
+		// given
+		String socialId = "12345";
+		SocialType socialType = KAKAO;
+		String email = "testtesttesttesttesttesttesttesttesttesttesttest"
+			+ "testtesttesttesttesttesttesttesttesttesttest@example.com";
+		String name = "프라이인드로스테쭈젠댄마리소피아수인레나테엘리자벳피아루이제";
+		LocalDate birthday = LocalDate.of(1990, 1, 1);
+		String gender = "남성";
+		boolean serviceAgreement = true;
+		boolean collectionAgreement = true;
+
+		MemberInfo memberInfo = MemberInfo.builder()
+			.email(email)
+			.name(name)
+			.birthday(birthday)
+			.gender(gender)
+			.serviceAgreement(serviceAgreement)
+			.collectionAgreement(collectionAgreement)
+			.build();
+
+		SocialInfo socialInfo = SocialInfo.builder()
+			.socialId(socialId)
+			.socialType(socialType)
+			.memberInfo(memberInfo)
+			.build();
+
+		given(socialConnectionRepository.existsMemberBySocialIdAndSocialType(socialId, socialType)).willReturn(false);
+
+		// when & then
+		assertThatThrownBy(() -> memberService.signUp(socialInfo))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(MEMBER_INVALID_EMAIL.getMessage());
+
+		// verify
+		then(socialConnectionRepository).should().existsMemberBySocialIdAndSocialType(socialId, socialType);
+		then(memberRepository).should(never()).save(any(Member.class));
+		then(socialConnectionRepository).should(never()).save(any(SocialConnection.class));
+	}
+
+	@Test
+	@DisplayName("이름 공백으로 인해 회원가입 실패")
+	void name_null_sign_up_over_fail() {
+		// given
+		String socialId = "12345";
+		SocialType socialType = KAKAO;
+		String email = "test@example.com";
+		String name = "";
+		LocalDate birthday = LocalDate.of(1990, 1, 1);
+		String gender = "남성";
+		boolean serviceAgreement = true;
+		boolean collectionAgreement = true;
+
+		MemberInfo memberInfo = MemberInfo.builder()
+			.email(email)
+			.name(name)
+			.birthday(birthday)
+			.gender(gender)
+			.serviceAgreement(serviceAgreement)
+			.collectionAgreement(collectionAgreement)
+			.build();
+
+		SocialInfo socialInfo = SocialInfo.builder()
+			.socialId(socialId)
+			.socialType(socialType)
+			.memberInfo(memberInfo)
+			.build();
+
+		given(socialConnectionRepository.existsMemberBySocialIdAndSocialType(socialId, socialType)).willReturn(false);
+
+		// when & then
+		assertThatThrownBy(() -> memberService.signUp(socialInfo))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(MEMBER_INVALID_NAME.getMessage());
+
+		// verify
+		then(socialConnectionRepository).should().existsMemberBySocialIdAndSocialType(socialId, socialType);
+		then(memberRepository).should(never()).save(any(Member.class));
+		then(socialConnectionRepository).should(never()).save(any(SocialConnection.class));
+	}
+
+	@Test
 	@DisplayName("이름 길이 초과로 인해 회원가입 실패")
 	void name_length_over_sign_up_over_fail() {
 		// given
@@ -71,6 +197,47 @@ class MemberServiceTest {
 		assertThatThrownBy(() -> memberService.signUp(socialInfo))
 			.isInstanceOf(IllegalArgumentException.class)
 			.hasMessageContaining(MEMBER_INVALID_NAME.getMessage());
+
+		// verify
+		then(socialConnectionRepository).should().existsMemberBySocialIdAndSocialType(socialId, socialType);
+		then(memberRepository).should(never()).save(any(Member.class));
+		then(socialConnectionRepository).should(never()).save(any(SocialConnection.class));
+	}
+
+	@Test
+	@DisplayName("성별 길이 초과로 인해 회원가입 실패")
+	void gender_length_over_sign_up_over_fail() {
+		// given
+		String socialId = "12345";
+		SocialType socialType = KAKAO;
+		String email = "test@example.com";
+		String name = "프라이인드로스테쭈젠댄마리소피아수인레나테엘리자벳피아루이제";
+		LocalDate birthday = LocalDate.of(1990, 1, 1);
+		String gender = "밝히고 싶지 않음";
+		boolean serviceAgreement = true;
+		boolean collectionAgreement = true;
+
+		MemberInfo memberInfo = MemberInfo.builder()
+			.email(email)
+			.name(name)
+			.birthday(birthday)
+			.gender(gender)
+			.serviceAgreement(serviceAgreement)
+			.collectionAgreement(collectionAgreement)
+			.build();
+
+		SocialInfo socialInfo = SocialInfo.builder()
+			.socialId(socialId)
+			.socialType(socialType)
+			.memberInfo(memberInfo)
+			.build();
+
+		given(socialConnectionRepository.existsMemberBySocialIdAndSocialType(socialId, socialType)).willReturn(false);
+
+		// when & then
+		assertThatThrownBy(() -> memberService.signUp(socialInfo))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining(MEMBER_INVALID_GENDER.getMessage());
 
 		// verify
 		then(socialConnectionRepository).should().existsMemberBySocialIdAndSocialType(socialId, socialType);
