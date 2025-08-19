94154497bdaa4772158d807ab3c4b536897e9dc9
fakerdeft <whakswp123@naver.com>
2025-08-19 14:38:50 +0900

    (#71)fix: 탈퇴 엔티티 추가 및 탈퇴 로직 수정

---
+++ src/main/java/com/dalcoomi/member/dto/request/WithdrawRequest.java
@@ -0,0 +1,21 @@
+package com.dalcoomi.member.dto.request;
+
+import java.util.List;
+
+import com.dalcoomi.member.domain.WithdrawalType;
+import com.dalcoomi.member.dto.LeaderTransferInfo;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record WithdrawRequest(
+	@NotNull
+	WithdrawalType withdrawalType,
+
+	@Size(max = 50, message = "기타 사유 내용은 최대 50자입니다.")
+	String otherReason,
+
+	List<LeaderTransferInfo> leaderTransferInfos
+) {
+
+}
