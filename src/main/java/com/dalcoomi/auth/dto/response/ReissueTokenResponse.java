2147bbe83ef0d0cda9c153e1e0821a0343b90b61
fakerdeft <whakswp123@naver.com>
2025-09-11 00:55:19 +0900

    (#83)refactor: 정적 팩토리 메서드 방식으로 수정

--- src/main/java/com/dalcoomi/auth/dto/response/ReissueTokenResponse.java
+++ src/main/java/com/dalcoomi/auth/dto/response/ReissueTokenResponse.java
@@ -1,8 +1,19 @@
 package com.dalcoomi.auth.dto.response;
 
+import com.dalcoomi.auth.dto.TokenInfo;
+
+import lombok.Builder;
+
+@Builder
 public record ReissueTokenResponse(
 	String accessToken,
 	String refreshToken
 ) {
 
+	public static ReissueTokenResponse from(TokenInfo tokenInfo) {
+		return ReissueTokenResponse.builder()
+			.accessToken(tokenInfo.accessToken())
+			.refreshToken(tokenInfo.refreshToken())
+			.build();
+	}
 }
