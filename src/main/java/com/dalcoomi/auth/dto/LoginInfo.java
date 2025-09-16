9acbf4473d4bf31ef829c6ae0996ad4e13575895
fakerdeft <whakswp123@naver.com>
2025-09-17 00:58:47 +0900

    (#83)fix: 다른 소셜 계정으로 로그인 시 기존 소셜 타입 응답 추가

--- src/main/java/com/dalcoomi/auth/dto/LoginInfo.java
+++ src/main/java/com/dalcoomi/auth/dto/LoginInfo.java
@@ -1,10 +1,13 @@
 package com.dalcoomi.auth.dto;
 
+import com.dalcoomi.member.domain.SocialType;
+
 import lombok.Builder;
 
 @Builder
 public record LoginInfo(
 	boolean sameSocial,
+	SocialType existingSocialType,
 	String accessToken,
 	String refreshToken
 ) {
--- src/main/java/com/dalcoomi/auth/dto/response/LoginResponse.java
+++ src/main/java/com/dalcoomi/auth/dto/response/LoginResponse.java
@@ -1,12 +1,14 @@
 package com.dalcoomi.auth.dto.response;
 
 import com.dalcoomi.auth.dto.LoginInfo;
+import com.dalcoomi.member.domain.SocialType;
 
 import lombok.Builder;
 
 @Builder
 public record LoginResponse(
 	boolean sameSocial,
+	SocialType existingSocialType,
 	String accessToken,
 	String refreshToken
 ) {
@@ -14,6 +16,7 @@ public record LoginResponse(
 	public static LoginResponse from(LoginInfo loginInfo) {
 		return LoginResponse.builder()
 			.sameSocial(loginInfo.sameSocial())
+			.existingSocialType(loginInfo.existingSocialType())
 			.accessToken(loginInfo.accessToken())
 			.refreshToken(loginInfo.refreshToken())
 			.build();
