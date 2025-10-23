3c15e15dd9ecebfecdb2050dead9a96e7853096f
fakerdeft <whakswp123@naver.com>
2025-10-23 10:57:26 +0900

    hotfix: v5 마이그레이션 파일 추가

---
+++ src/main/resources/db/migration/V5__add_team_member_display_order_column.sql
@@ -0,0 +1,7 @@
+-- V5: team_member 테이블에 display_order 컬럼 추가
+-- 그룹 목록 정렬 순서를 사용자가 지정할 수 있도록 display_order 컬럼 추가
+
+ALTER TABLE team_member ADD COLUMN display_order INT NULL COMMENT '그룹 표시 순서';
+
+-- 기존 데이터에 대한 display_order 값 설정 (0으로 초기화)
+UPDATE team_member SET display_order = 0 WHERE display_order IS NULL;
